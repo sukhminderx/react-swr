ae08f66938c6aff5c4d08531cd3577519ba1fa4f
gabruism <sukhminderxliterature@gmail.com>
2022-07-26 17:08:19 +0530

    swr

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useSWR from 'swr';
 
+
+const fetcher = (...args: Array<any>) => fetch(args[0]).then(response => {
+  if (!response.ok) {
+    throw new Error("HTTP error " + response.status);
+}
+return response.json();
+});
 function App() {
+  const {data, error} = useSWR('/res.json',fetcher, {refreshInterval: 2} )
+
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
